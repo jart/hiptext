bf0348de726231b7d0e33dba3cc27397cd6851ac
Justine Alexandra Roberts Tunney <jtunney@gmail.com>
2015-06-28 23:52:14 -0400

    Add editor style comments to artiste.{cc,h}.

--- artiste.cc
+++ artiste.cc
@@ -239,3 +239,14 @@ void Artiste::ShowCursor() {
 void Artiste::ResetCursor() {
   output_ << "\x1b[H";     // ANSI put cursor in top left.
 }
+
+// For Emacs:
+// Local Variables:
+// mode:c++
+// indent-tabs-mode:nil
+// tab-width:2
+// c-basic-offset:2
+// c-file-style: nil
+// End:
+// For VIM:
+// vim:set expandtab softtabstop=2 shiftwidth=2 tabstop=2:
--- artiste.h
+++ artiste.h
@@ -53,3 +53,14 @@ class Artiste {  // The one who lives in your terminal.
 };
 
 #endif  // HIPTEXT_ARTISTE_H_
+
+// For Emacs:
+// Local Variables:
+// mode:c++
+// indent-tabs-mode:nil
+// tab-width:2
+// c-basic-offset:2
+// c-file-style: nil
+// End:
+// For VIM:
+// vim:set expandtab softtabstop=2 shiftwidth=2 tabstop=2:
